77e4cf46ed8c5c163f877d5aec0603690682cf94
ayoubqrt <moustakim.ayoub@hotmail.com>
2022-12-15 10:47:30 +0100

    added eslint + prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0);
 
-  return (
-    <div>
-      <div >
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    </div>
-  )
+	return (
+		<div>
+			<h1>To do app</h1>
+			<div>
+				<button onClick={() => setCount((count) => count + 1)}>
+					count is {count}
+				</button>
+			</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
